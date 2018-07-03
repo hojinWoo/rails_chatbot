77c893e6873f172819cc0aff760e9a8ed75c14fc
ChangHee <och8808@naver.com>
2018-07-03 02:21:58 +0000

    메아리

--- app/controllers/kakao_controller.rb
+++ app/controllers/kakao_controller.rb
@@ -10,5 +10,20 @@ class KakaoController < ApplicationController
   end
   
   def message
+    @user_msg = params[:content]
+    
+    @return_msg ={
+      :text => @user_msg
+    }
+    @return_keyboard = {
+        :type => "buttons",
+        :buttons => ["메뉴", "로또", "고양이"]
+    }
+    
+    @result = {
+      :message => @return_msg,
+      :keyboard => @return_keyboard
+    }
+    render json: @result
   end
 end
