642e16599f144a533bf09ed52e9a0aee32b4e88e
ChangHee <och8808@naver.com>
2018-07-03 03:22:02 +0000

    menu

--- app/controllers/kakao_controller.rb
+++ app/controllers/kakao_controller.rb
@@ -11,9 +11,16 @@ class KakaoController < ApplicationController
   
   def message
     @user_msg = params[:content]
+    @text = "기본응답"
+    if @user_msg == "메뉴"
+      @text = ["20층","멀캠식당","급식"].sample
+    elsif @user_msg == "로또"
+      @text = (1..45).to_a.sample(6).sort.to_s
+    end
+    
     
     @return_msg ={
-      :text => @user_msg
+      :text => @text
     }
     @return_keyboard = {
         :type => "buttons",
